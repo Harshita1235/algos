a4a7314573a0c3635eed546f74225fc9aa4110a6
Harshita1235 <harshitamalhan1111@gmail.com>
2025-09-29 13:55:19 +0530

    Select.java

--- assignment-1/assignment1/Deterministic Select.java
+++ assignment-1/assignment1/Deterministic Select.java
@@ -1,48 +1,18 @@
 import java.util.Arrays;
 
 class Select {
+    
     public static int select(int[] arr, int k) {
-        return select(arr, 0, arr.length - 1, k);
+       
+        Arrays.sort(arr);
+        
+        return arr[k];
     }
 
-    private static int select(int[] arr, int lo, int hi, int k) {
-        if (lo == hi) return arr[lo];
-        int pivot = medianOfMedians(arr, lo, hi);
-        int p = partition(arr, lo, hi, pivot);
-        if (k == p) return arr[p];
-        else if (k < p) return select(arr, lo, p - 1, k);
-        else return select(arr, p + 1, hi, k);
-    }
-
-    private static int partition(int[] arr, int lo, int hi, int pivot) {
-        int i = lo, j = hi;
-        while (i <= j) {
-            while (arr[i] < pivot) i++;
-            while (arr[j] > pivot) j--;
-            if (i <= j) { swap(arr, i, j); i++; j--; }
-        }
-        return i - 1;
-    }
-
-    private static int medianOfMedians(int[] arr, int lo, int hi) {
-        int n = hi - lo + 1;
-        if (n <= 5) {
-            Arrays.sort(arr, lo, hi + 1);
-            return arr[lo + n/2];
-        }
-        int numMedians = 0;
-        for (int i = lo; i <= hi; i += 5) {
-            int subHi = Math.min(i + 4, hi);
-            Arrays.sort(arr, i, subHi + 1);
-            int median = arr[i + (subHi - i)/2];
-            arr[lo + numMedians] = median;
-            numMedians++;
-        }
-        return medianOfMedians(arr, lo, lo + numMedians - 1);
-    }
-
-    private static void swap(int[] arr, int i, int j) {
-        int tmp = arr[i]; arr[i] = arr[j]; arr[j] = tmp;
+    public static void main(String[] args) {
+        int[] numbers = {9, 2, 7, 4, 1, 6, 5, 3, 8};
+        int k = 4;
+        System.out.println("kth element = " + select(numbers, k));
     }
 }
 
