ef13feffd23c00a0b1b0c902c9c9cf26fc4e5eb4
Harshita1235 <harshitamalhan1111@gmail.com>
2025-09-29 13:26:48 +0530

    Main.java

--- assignment-1/assignment1/Main.java
+++ assignment-1/assignment1/Main.java
@@ -2,27 +2,23 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int n = 10000; // size of test array
+        int n = 10000; 
         Random rand = new Random();
+     int[] arr = rand.ints(n, 0, 100000).toArray();
 
-        // Prepare a random array
-        int[] arr = rand.ints(n, 0, 100000).toArray();
-
-        // ==== Test MergeSort ====
-        int[] arr1 = arr.clone();
+     int[] arr1 = arr.clone();
         long start = System.nanoTime();
         MergeSort.sort(arr1);
         long end = System.nanoTime();
         System.out.println("MergeSort time: " + (end - start)/1e6 + " ms");
 
-        // ==== Test QuickSort ====
+      
         int[] arr2 = arr.clone();
         start = System.nanoTime();
         QuickSort.sort(arr2);
         end = System.nanoTime();
         System.out.println("QuickSort time: " + (end - start)/1e6 + " ms");
 
-        // ==== Test Deterministic Select (median element) ====
         int[] arr3 = arr.clone();
         start = System.nanoTime();
         int kth = DeterministicSelect.select(arr3, arr3.length/2);
@@ -30,7 +26,6 @@ public class Main {
         System.out.println("Deterministic Select (median): " + kth +
                            ", time: " + (end - start)/1e6 + " ms");
 
-        // ==== Test Closest Pair ====
         ClosestPair.Point[] pts = new ClosestPair.Point[n];
         for (int i = 0; i < n; i++) {
             pts[i] = new ClosestPair.Point(rand.nextDouble()*1000, rand.nextDouble()*1000);
@@ -42,3 +37,4 @@ public class Main {
                            ", time: " + (end - start)/1e6 + " ms");
     }
 }
+
