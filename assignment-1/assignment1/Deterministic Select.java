5b870abf94def4198070470bb361b52a6ab11af0
Harshita1235 <harshitamalhan1111@gmail.com>
2025-09-29 14:41:26 +0530

    Select.java

--- assignment-1/assignment1/Deterministic Select.java
+++ assignment-1/assignment1/Deterministic Select.java
@@ -1,18 +1,17 @@
 import java.util.Arrays;
 
-class Select {
+class DeterministicSelectEasy {
+
     
     public static int select(int[] arr, int k) {
-       
         Arrays.sort(arr);
-        
-        return arr[k];
+        return arr[k];     
     }
 
     public static void main(String[] args) {
         int[] numbers = {9, 2, 7, 4, 1, 6, 5, 3, 8};
-        int k = 4;
-        System.out.println("kth element = " + select(numbers, k));
+        int k = 4; 
+        System.out.println("Original array: " + Arrays.toString(numbers));
+        System.out.println("k-th smallest element = " + select(numbers, k));
     }
 }
-
